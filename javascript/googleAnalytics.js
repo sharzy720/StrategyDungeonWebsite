9f2c3e9f643f62f2f0e95237b6250c0d03c4163a
Johnathyn Strong <johnathynstrong@gmail.com>
2022-03-26 00:02:38 -0400

    Code for adding Google Analytics to website

---
+++ javascript/googleAnalytics.js
@@ -0,0 +1,18 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+src="https://www.googletagmanager.com/gtag/js?id=G-22HN7L2Z73"
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-22HN7L2Z73');
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-22HN7L2Z73"></script>
+// <script>
+//     window.dataLayer = window.dataLayer || [];
+//     function gtag(){dataLayer.push(arguments);}
+//     gtag('js', new Date());
+//
+//     gtag('config', 'G-22HN7L2Z73');
+// </script>
